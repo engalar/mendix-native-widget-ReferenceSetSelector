4f53c5126150526636b5250c75cbb41d766f3d4c
engalar <engalar@gmail.com>
2022-03-24 17:53:48 +0800

    static component

--- src/ReferenceSetSelector.tsx
+++ src/ReferenceSetSelector.tsx
@@ -1,35 +1,100 @@
-import { createElement, ReactElement, useCallback } from "react";
+import { createElement, ReactElement, useRef, useState } from "react";
 import { ReferenceSetSelectorProps } from "../typings/ReferenceSetSelectorProps";
-import { Pressable, Text, View } from "react-native";
-import { defaultReferenceSetSelectorStyle, ReferenceSetSelectorStyle } from "./ui/Styles";
-import { flattenStyles } from "./piw-native-utils-internal";
-import { executeAction } from "./piw-utils-internal";
+import { View } from "react-native";
+import { ReferenceSetSelectorStyle } from "./ui/Styles";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import MultiSelect from 'react-native-multiple-select';
+
+const defaultSearchIcon = (
+    <Icon
+        name="magnify"
+        size={20}
+        color="#A9A9A9"
+        style={{ marginRight: 10 }}
+    />
+);
+
+const items = [
+    { id: '2iijs7yta', name: 'Ondo' },
+    { id: 'iijs7yta', name: 'Ondo' },
+    { id: 'ijs7yta', name: 'Ondo' },
+    { id: 'js7yta', name: 'Ondo' },
+    { id: 's7yta', name: 'Ondo' },
+    { id: '1', name: 'Ondo' },
+    { id: '2', name: 'Ondo' },
+    { id: '3', name: 'Ondo' },
+    { id: '4', name: 'Ondo' },
+    { id: '5', name: 'Ondo' },
+    { id: '6', name: 'Ondo' },
+    { id: '8', name: 'Ondo' },
+    { id: '9', name: 'Ondo' },
+    { id: '11', name: 'Ondo' },
+    { id: '12', name: 'Ondo' },
+    {
+        id: 'a0s0a8ssbsd',
+        name: 'Ogun'
+    }, {
+        id: '16hbajsabsd',
+        name: 'Calabar'
+    }, {
+        id: 'nahs75a5sg',
+        name: 'Lagos'
+    }, {
+        id: '667atsas',
+        name: 'Maiduguri'
+    }, {
+        id: 'hsyasajs',
+        name: 'Anambra'
+    }, {
+        id: 'djsjudksjd',
+        name: 'Benue'
+    }, {
+        id: 'sdhyaysdj',
+        name: 'Kaduna'
+    }, {
+        id: 'suudydjsjd',
+        name: 'Abuja'
+    }
+];
 
 export function ReferenceSetSelector(props: ReferenceSetSelectorProps<ReferenceSetSelectorStyle>): ReactElement {
-    const styles = flattenStyles(defaultReferenceSetSelectorStyle, props.style);
+    console.log(props);
 
-    const renderText = useCallback(() => {
-        return (
-            <Text testID={`${props.name}$caption`} style={styles.caption}>
-                demo
-            </Text>
-        );
-    }, [props.caption, styles]);
+    const ref = useRef<MultiSelect>(null);
+    const [selectedItems, setselectedItems] = useState<any>([]);
 
     return (
-        <View style={styles.container} testID={props.name}>
-            {props.onClick ? (
-                <Pressable
-                    onPress={() => executeAction(props.onClick)}
-                    android_ripple={{
-                        color: styles.container.rippleColor
-                    }}
-                >
-                    {renderText()}
-                </Pressable>
-            ) : (
-                renderText()
-            )}
+        <View style={{ flex: 1 }}>
+            <MultiSelect
+                searchIcon={defaultSearchIcon}
+                hideTags
+                items={items}
+                uniqueKey="id"
+                ref={ref}
+                onSelectedItemsChange={e => {
+                    setselectedItems(e);
+                    console.log(e);
+                }}
+                selectedItems={selectedItems}
+                selectText="Pick Items"
+                searchInputPlaceholderText="Search Items..."
+                onChangeInput={(text) => console.log(text)}
+                altFontFamily="ProximaNova-Light"
+                tagRemoveIconColor="#CCC"
+                tagBorderColor="#CCC"
+                tagTextColor="#CCC"
+                selectedItemTextColor="#CCC"
+                selectedItemIconColor="#CCC"
+                itemTextColor="#000"
+                displayKey="name"
+                searchInputStyle={{ color: '#CCC' }}
+                submitButtonColor="#CCC"
+                submitButtonText="Submit"
+                styleListContainer={{ height: 90 }}
+            />
+            <View>
+                {ref.current && ref.current.getSelectedItemsExt(selectedItems)}
+            </View>
         </View>
     );
 }
